8766323413dd95cdf4d03deae41ac73ac9a7095c
Kavia AI <production@kavia.ai>
2025-08-06 19:23:17 +0000

    CheckPoint - cmd4b5eb3e

--- domains/search/SearchFilters.jsx
+++ domains/search/SearchFilters.jsx
@@ -4,9 +4,9 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
-import { filterData, getFilterValues } from '../utils/filterData';
-import { baseUrl, fetchApi } from '../utils/fetchApi';
-import noresult from '../assets/images/noresult.svg';
+import { filterData, getFilterValues } from './filterData';
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+import noresult from '../../assets/images/noresult.svg';
 
 export default function SearchFilters() {
   const [filters] = useState(filterData);
